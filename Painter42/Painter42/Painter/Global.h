90551a1e857a0bc2ba30e0be4a798d0273d8e6bc
a1m1ng <51517520+a1m1ng@users.noreply.github.com>
2024-12-12 20:08:43 +0200

    reload

--- Painter42/Painter42/Painter/Global.h
+++
@@ -1,5 +0,0 @@
-// файл Global.cpp
-////////////////////////////////////////
-// Прототипы глобальных функций
-CPoint Transform(const CPoint &point, const CPoint &point0, double ang, int a, int b);
-double ZFunction(double x, double y);
